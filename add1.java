8ac1274a9fab87f13a17bcbbe4dae31cd2b8c800
alishasinghkritter <alisha@kritter.in>
2021-01-19 15:52:50 +0530

    1st

--- add1.java
+++ add1.java
@@ -2,8 +2,8 @@ public class AddTwoIntegers {
 
     public static void main(String[] args) {
         
-        int first = 25;
-        int second = 20;
+        int first = 30;
+        int second = 25;
 
         System.out.println("Enter two numbers: " + first + " " + second);
         int sum = first + second;
@@ -11,6 +11,7 @@ public class AddTwoIntegers {
         System.out.println("The sum is: " + sum);
     	
 	System.out.println("The multiply is : " +(first*second));
+	System.out.println("The subtraction is : "+(first-second));
 	System.out.println("Hello");
 	}
 }
